d3129068cc326a8fabdc780f9564eae6369e0f78
Administrator <1009333126>
2020-04-27 20:44:04 +0800

    4.27commit

--- first-week.py
+++ first-week.py
@@ -1,10 +1,10 @@
-print("1. 列表、字符串、元组、字典之间的相互转换")
+"1. 列表、字符串、元组、字典之间的相互转换"
+print("1.列表、字符串、元组、字典之间的相互转换")
 str1 = "hello world"
 list1 = [11, 22, 33, 44]
 list2 = ['aa', 'bb', 'cc', 'dd']
 tuple1 = (1, 2, 3, 4)
 dict1 = {'name': 'zhangsan', 'age': 18, 'gender': 'boy'}
-
 print('字符串转列表：',list(str1))
 print('字符串转列表：',str1.split())
 print("元组转列表：",list(tuple1))
@@ -18,11 +18,11 @@ print("字符串转字典：dict = eval(str)")
 print(str(list1))
 print(type(str(list1)))
 
-
+"2.列表的遍历方式"
 print("2.列表的遍历方式")
 #列表的增加元素list.append('haha') 默认增加到末尾
 #列表插入元素到指定位置list.insert(i,'add') i为索引
-#列表删除元素list.pop（）删除末尾元素，list.pop(i)删除索引为i的元素
+#列表删除元素list.pop（）删除末尾元素，list.pop(i)删除索引为i的元素,list.remove(haha)直接删除这个元素
 #替换元素 list[i] = 'haha' 替换索引为i的元素
 print('方法一')
 for i in list2:
@@ -37,3 +37,48 @@ i = 0
 while i < int(len(list2)):
     print(list2[i])
     i += 1
+
+
+"3. 字符串截取子串"
+print("3. 字符串截取子串")
+str1 = 'aaabbbcccdddeeefff'
+print('字符串截取子串：',str1[3:9])
+print('字符串截取子串：',str1[::3])
+print('字符串截取子串：',str1[-1:-7:-1])
+
+"4. 字典的遍历方式"
+print("4. 字典的遍历方式")
+# 字典的遍历方式
+dict1 = {'name': 'zhangsan', 'age': 18, 'gender': 'boy'}
+
+print('***打印key***')
+for i in dict1.keys():
+    print(i)
+
+print('***打印value***')
+for i in dict1.values():
+    print(i)
+
+print('***打印(key,value)***')
+for i in dict1.items():
+    print(i)
+
+print('***打印key:value***')
+for k,v in dict1.items():
+    print(k,':',v)
+
+print("5-字典的增加、删除、更新")
+# 字典的增加、删除、更新
+dict1 = {'name': 'zhangsan', 'age': 18, 'gender': 'boy'}
+
+dict1['Class'] = 'First'
+print('新增后的字典：',dict1)
+
+dict1['age'] = 16
+print('更新后的字典：',dict1)
+
+del dict1['name']   # 删除键name
+print('del删除后的字典：',dict1)
+
+dict1.clear()    #清空字典
+print('clear清空后的字典：',dict1)
