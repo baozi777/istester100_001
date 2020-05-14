fa0eaa2208bdeb2e26b26b21636d4a8381161d9f
Administrator <1009333126>
2020-05-14 09:38:35 +0800

    5.14commit task

--- May-task.py
+++ May-task.py
@@ -19,23 +19,35 @@
 #       break
 # print(b,count_swap,count)
 
-"""
-求十万内的素数，并打印运算次数，运算时间，正确情况下效率越高越好
-"""
-print(2)
-count = 1
-sum = 0
-import time
-start = time.perf_counter()
-for i in range(3,100000,2):#进入循环的都是奇数
-    for n in range(3,int(i**0.5)+1,2):#奇数除偶数不能整除，所以跳过
-        sum += 1
-        if i % n == 0 :
-            break
-    else:
-        print(i)
-        count+=1
-end = time.perf_counter()
-print('一共有%d个素数'%count)
-print('算了%d次'% sum)
-print('run:%ss'%(end-start))
+# """
+# 求十万内的素数，并打印运算次数，运算时间，正确情况下效率越高越好
+# """
+# print(2)
+# count = 1
+# sum = 0
+# import time
+# start = time.perf_counter()
+# for i in range(3,100000,2):#进入循环的都是奇数
+#     for n in range(3,int(i**0.5)+1,2):#奇数除偶数不能整除，所以跳过
+#         sum += 1
+#         if i % n == 0 :
+#             break
+#     else:
+#         print(i)
+#         count+=1
+# end = time.perf_counter()
+# print('一共有%d个素数'%count)
+# print('算了%d次'% sum)
+# print('run:%ss'%(end-start))
+# """
+# 汉诺塔
+# """
+# def move(n,a,b,c):
+#     if n == 1:
+#         print(a, '-->', c)
+#     else:
+#         move(n - 1, a, c, b)
+#         print(a, '-->', c)
+#         move(n - 1, b, a, c)
+# move(3,'A','B','C')
+
